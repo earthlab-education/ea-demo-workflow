66cb60484ec753d41dbea7d11c425f3cebe35066
nkorinek <nako1890@colorado.edu>
2021-05-14 14:07:29 -0600

    minor update to bash

--- code/main.sh
+++ code/main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jupyter nbconvert --to "notebook" --inplace --execute */process-data.ipynb
-jupyter nbconvert --to "html" --no-input --execute */blog.ipynb
+jupyter nbconvert --to notebook --inplace --execute */process-data.ipynb
+jupyter nbconvert --to html --no-input --execute */blog.ipynb
